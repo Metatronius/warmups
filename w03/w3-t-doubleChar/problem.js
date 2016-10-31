a6b51e109f8de202453e3c91a0705e0fec62aea0
Paul E Lederer <plederer93@gmail.com>
2016-10-31 09:52:25 -0500

    doubChar

--- w03/w3-t-doubleChar/problem.js
+++ w03/w3-t-doubleChar/problem.js
@@ -1,6 +1,6 @@
 // Given a string, you have to return a string in which
 // each character (case-sensitive) is repeated once.
-// if anything but a string is passed to the function, 
+// if anything but a string is passed to the function,
 // return "Ruh roh! That isn't a string!"
 
 // doubleChar("String") ==> "SSttrriinngg"
@@ -13,10 +13,18 @@
 // DONE?? Do it with a RegEx! (a 2 line solution is possible)
 
 function doubleChar(str) {
-
+  var returnable = '';
+  if (typeof str === 'string'){
+    for (var i = 0; i < str.length; i++) {
+      returnable += str.charAt(i);
+      returnable += str.charAt(i);
+    }
+    return returnable;
+  }
+return "Ruh roh! That isn't a string!";
 }
 
 module.exports = {
   doubleChar:doubleChar,
   attendance:"wordy word here"
-}
+};
